81c95f8f7cbc2b0eb09a81597a58e06e0a9baf08
DhruvTemura <dhruvtemura@gmail.com>
2025-06-02 18:16:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+const BhosduButton = () => {
+  return(
+    <button>Click me</button>
+  )
+}
+
+
+const App = () => {
+  return(
+    <>
+      <h2>Dhruv Temura</h2>
+      <BhosduButton></BhosduButton>
+    </>
+  )
+}
+
+export default App
