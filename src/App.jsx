7c47a43955206f14ebd18da0ae4e5bfeb6db4308
DhruvTemura <dhruvtemura@gmail.com>
2025-06-02 20:38:23 +0530

    demo state

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
+import { useState } from 'react'
 import './App.css'
 
 const Card = ({title}) => {
   return(
-    <p>{title}</p>
+    <div className='card'>
+      <h2>{title}</h2>
+    </div>
+    
   )
 }
 
 
 const App = () => {
+
+  const [hasLiked, setHasLiked] = useState()
+
   return(
-    <>
-      <h2>Dhruv Temura</h2>
+    <div className='card-container'>
       <Card title='dead reckoning'/>
       <Card title='conjuring 4'/>
-    </>
+      <Card title='final destination'/>
+    </div>
   )
 }
 
