3da8079d14ace0b5e83ec1f9b7165716ca46cf57
DhruvTemura <dhruvtemura@gmail.com>
2025-06-03 03:01:31 +0530

    searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import Search from './components/Search';
+import {useState} from 'react'
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <main>
       <div className='pattern'></div>
@@ -10,6 +15,8 @@ const App = () => {
           <img src='./hero.png' alt='hero banner'></img>
           <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
         </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   )
