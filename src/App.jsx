909cb7f5ffffa5f7cc20cdcae261bc2ca0621b90
DhruvTemura <dhruvtemura@gmail.com>
2025-06-03 01:40:58 +0530

    tailwind integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,9 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-
-const Card = ({title}) => {
-
-  const [count, setCount] = useState(0);
-
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log('hi');
-  }, [hasLiked]);
-
-  return(
-    <div className='card' onClick={() => setCount(count + 1)}>
-      <h2>{title} <br /> {count}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? '❤️' : '👍'}
-      </button>
-    </div>
-    
-  )
-}
-
+import React from 'react'
 
 const App = () => {
-
-  
-
-  return(
-    <div className='card-container'>
-      <Card title='dead reckoning'/>
-      <Card title='conjuring 4'/>
-      <Card title='final destination'/>
-    </div>
+  return (
+    <div>App</div>
   )
 }
 
-export default App
+export default App;
