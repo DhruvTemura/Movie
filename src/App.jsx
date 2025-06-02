32cea219beb8cdf23008bd3a3494e9b2030210cb
DhruvTemura <dhruvtemura@gmail.com>
2025-06-03 02:02:10 +0530

    images added and components folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1>Dhruv</h1>
-    </div>
+    <main>
+      <div className='pattern'></div>
+
+      <div className='wrapper'>
+        <header>
+          <img src='./hero.png' alt='hero banner'></img>
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+      </div>
+    </main>
   )
 }
 
