27d1f56366f26c19bf45359a04a920384102307d
DhruvTemura <dhruvtemura@gmail.com>
2025-06-04 21:30:12 +0530

    api added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import Search from './components/Search';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'applications/json',
+    Authorization: `Bearer${API_KEY}`
+  }
+}
 
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useState('');
+
+  const fetchMovies = async () => {
+    try{
+
+    } catch(error){
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Error fetching movies. Please try again later');
+    }
+  }
+
+  useEffect(() => {
+
+  },[]);
 
   return (
     <main>
@@ -14,9 +39,16 @@ const App = () => {
         <header>
           <img src='./hero.png' alt='hero banner'></img>
           <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section className='all-movies'>
+          <h2>All Movies</h2>
+          {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+        </section>
+
+        
       </div>
     </main>
   )
